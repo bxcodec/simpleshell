e0006c1bd74006167e5aee480bd2921125569cf9
Iman Tumorang <bxcodec@gmail.com>
2018-11-07 15:45:34 +0700

    store to github

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$ ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		err = runCommand(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+func runCommand(commandStr string) error {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
+		// add another case here for custom commands.
+	}
+	cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
+}
